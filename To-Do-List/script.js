6994db26ca5ea3387f8b2ece5b42c9d45dab9aeb
Graudss <yusuf.guner.766@gmail.com>
2024-02-20 00:27:55 +0300

    update

---
+++ To-Do-List/script.js
@@ -0,0 +1,35 @@
+const inputBox= document.getElementById("input-box");
+const inputContainer= document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ""){
+        alert("Boş Değer Ekleyemezsin!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML=inputBox.value;
+        inputContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span); 
+    }
+    inputBox.value="";
+    saveData();
+}
+
+inputContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", inputContainer.innerHTML)
+}
+function showeTask(){
+    inputContainer.innerHTML = localStorage.getItem("data");
+}
+showeTask();
